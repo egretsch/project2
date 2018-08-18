b82d8e08a9ac0bdbcb8a191357136dd9e212dfe1
Bob Ross <ministash14@gmail.com>
2018-08-18 15:11:32 -0700

    Importing Chance

--- app/storedArticlesApi.js
+++
@@ -1,8 +0,0 @@
-let scrapedNewYorTimesInfo = [
-
-      
-];
-
-
-
-module.exports = scrapedNewYorTimesInfo;
--- models/todo.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Todo = sequelize.define("Todo", {
-    text: DataTypes.STRING,
-    complete: DataTypes.BOOLEAN
-  });
-  return Todo;
-};
