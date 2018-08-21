fb59ca9cda774671edfc4debc56f709c22fdb6c4
Bob Ross <ministash14@gmail.com>
2018-08-21 14:59:41 -0700

    Made a button, added some logic

--- public/assets/js/postsLogic.js
+++ public/assets/js/postsLogic.js
@@ -1,6 +1,7 @@
 $(".posts-articles-individual").on('click', function(){
    let postsUserArticleId = $(this).attr('data');
    window.location.href = "/userarticle/" + postsUserArticleId;
+   console.log(postsUserArticleId);
 
    $.get('/userarticle' + postsUserArticleId, function(req, res){
        console.log("I sent something possibly");
