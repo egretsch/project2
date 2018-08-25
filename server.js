fb90ef3fe9312968a3f7247dd80a861ff6945ee3
chancemusselman <musselman.chance@icloud.com>
2018-08-25 05:26:04 -0700

    Trying again

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
-const app = express();
+
+let app = express();
 let PORT = process.env.PORT || 8080;
+
 
 let db = require("./models");
 
@@ -14,8 +16,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-require("./routes/api-routes.js")(app);
 
+require("./routes/api-routes.js")(app);
 
 
 db.sequelize.sync({ force: false }).then(function() {
