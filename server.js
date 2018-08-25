84a006f844df90b474693fb1010ba7ea51feb79f
chancemusselman <musselman.chance@icloud.com>
2018-08-25 01:19:08 -0700

    heroku test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var methodOverride = require('method-override');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
+// app.use(methodOverride('_method'));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // app.use(articleRoutes);
