f47c375d2447e45edabded9b6baeefaa038ff1db
Bob Ross <ministash14@gmail.com>
2018-08-24 21:44:35 -0700

    Heroku stuff

--- server.js
+++ server.js
@@ -3,32 +3,13 @@ var session = require('express-session');
 var app = express();
 var bodyParser = require("body-parser");
 var db = require("./models");
-let cors = require('cors');
-// Set Handlebars.
 var exphbs = require("express-handlebars");
-var PORT = process.env.PORT || 3000;
 let articleRoutes = require('./routes/api-routes.js');
 
-// app.use(session({
-//   secret: process.env.SESSIONSECRET || 'cat',
-//   resave: false,
-//   saveUninitialized: true
-// }));
 
-// function userSetup(req, res, next) {
-//   if (!req.session.user) {
-//     req.session.user = {};
-//     req.session.user.loggedIn = false;
-//   }
-//   next();
-// }
-
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(cors());
-// app.use(userSetup);
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(articleRoutes);
@@ -38,6 +19,7 @@ app.use(articleRoutes);
 
 
 
+var PORT = process.env.PORT || 3000;
 // Start our server so that it can begin listening to client requests.
 db.sequelize.sync({ force: false }).then(function () {
   app.listen(PORT, function () {
